c7f775a520dddeba1ad545fa1e0031f3aba8a196
Rohit72099 <rk464152@gmail.com>
2025-07-06 18:47:55 +0530

    minor bug fixed 4

--- src/app/api/transactions/[id]/route.ts
+++ src/app/api/transactions/[id]/route.ts
@@ -2,25 +2,33 @@ import { connectToDatabase } from "@/lib/db";
 import { Transaction } from "@/models/Transaction";
 import { NextResponse } from "next/server";
 
-// PUT
-export async function PUT(
-  req: Request,
-  context: { params: any } 
-) {
-  await connectToDatabase();
-  const body = await req.json();
-  const { id } = context.params;
-  const updated = await Transaction.findByIdAndUpdate(id, body, { new: true });
-  return NextResponse.json(updated);
-}
 
-// DELETE
+type TransactionUpdate = {
+  amount?: number;
+  description?: string;
+  date?: string;
+  category?: string;
+};
+
+
 export async function DELETE(
   req: Request,
-  context: { params: any } 
+  context: { params: { id: string } }
 ) {
   await connectToDatabase();
   const { id } = context.params;
   await Transaction.findByIdAndDelete(id);
   return NextResponse.json({ message: "Deleted" });
+}
+
+
+export async function PUT(
+  req: Request,
+  context: { params: { id: string } }
+) {
+  await connectToDatabase();
+  const body: TransactionUpdate = await req.json();
+  const { id } = context.params;
+  const updated = await Transaction.findByIdAndUpdate(id, body, { new: true });
+  return NextResponse.json(updated);
 }
